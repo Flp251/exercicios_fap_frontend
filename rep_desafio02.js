73b08ea0b325739b23eaa0730706b0f4940e7797
Flp251 <139509960+Flp251@users.noreply.github.com>
2023-07-25 18:59:12 -0300

    Update rep_desafio02.js

--- rep_desafio02.js
+++ rep_desafio02.js
@@ -1,3 +1,16 @@
+
 var fruta = "";
 var caractere;
 var numero = 0;
+
+fruta = prompt ("qual a fruta escolhida? ");
+caractere = prompt ("caractere ");
+numero = prompt ("quantas repetições sera desejada? ");
+
+
+
+do {
+  console.log (" contar " + fruta)
+  
+  
+}while (numero <= 3);
